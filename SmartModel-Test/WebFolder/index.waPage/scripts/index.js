7277dc5020d5d685d97dc26acc3822d06e415c69
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-07-22 13:27:38 +0200

    update smartModel module

---
+++ SmartModel-Test/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,28 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var buttonRunTests = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	buttonRunTests.click = function buttonRunTests_click (event)// @startlock
+	{// @endlock
+		var result;
+		try {
+		    result = testSmartModel.runTests();
+    	    console.info(result);
+    		testLogs = result.data;
+    		if (testLogs)
+        		$('errorDivTestFailed').html(result.data.nbFailed ? result.data.nbFailed + ' tests failed' : '');
+    		sources.testLogs.sync();
+    	} catch (e) {
+    	    console.error(e);
+    	}
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("buttonRunTests", "click", buttonRunTests.click, "WAF");
+// @endregion
+};// @endlock
