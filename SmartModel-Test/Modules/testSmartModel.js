77b0a8573b5fbc730abd5180e770e6c9ffd613b8
Alexandre Morgaut <alexandre.morgaut@gmail.com>
2014-07-22 13:11:47 +0200

    add a RPC callable test module

---
+++ SmartModel-Test/Modules/testSmartModel.js
@@ -0,0 +1,15 @@
+﻿
+/**
+ * @method runTests
+ * @param {string} [test] optionnal specific test name (by default all tests are launched
+ * @returns {object}
+ **/
+exports.runTests = function runTests(test) {
+    var testSuite = require('smartModel/tests');
+    testSuite = testSuite['test ' + test.toUpperCase()] || testSuite;
+    var nbFails = require('test').run(testSuite);
+	return {
+	    nbFails: nbFails,
+	    logs: console.content
+	};
+};
